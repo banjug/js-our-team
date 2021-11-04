8e14d24bedca596451a458b452c84f340d9eb0ac
Samuele <miotto.samuele@gmail.com>
2021-11-04 15:23:18 +0100

    pseudocode

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+RICREARE IL LAYOUT
+1. creare un array con i dipendenti
+    array di oggetti (dipendente1, dipendente2, ecc)
+    ogni oggetto contiene nome, posizione e path dell'immagine (chiavi dell'oggetto)
+2. inserire le card dei dipendenti riprendendo i valori per ogni dipendente dall'array
+    una funzione che inserisce l'html nella pagina
+    la funzione riprende i dati dall'array di oggetti
+3. all'inserimento dei dati nel form salvarli come nuovo dipendente nell'array
+    push nell'array di un nuovo oggetto (dipendenteN) contentente i dati salvati dal form
+4. alla pressione del pulsante far apparire una nuova card con un nuovo dipendente
+*/
